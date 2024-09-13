e6d5f9c15635990509f78be6fe42bd1fc5db26d8
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2024-09-13 01:41:20 +0100

    Add silly benchmark

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 use rust_wiki::stage::stage_type::get_st_obj;
 
+// fn benchmark_stage_type(){
+//     let start = Instant::now();
+//     const iterations: usize = 1000;
+//     for i in 0..iterations{
+//         for j in 0..iterations{
+//             StageType::new(&format!("stageRT{i:03}_{j:02}.csv"));
+//         }
+//     }
+//     println!("{}", start.elapsed().as_secs_f64())
+// }
+
 fn main() {
     println!("{:?}", get_st_obj("sol 0 0"))
 }
