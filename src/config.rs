bb6dd27795968055018040c7d7862ec5e1cd8288
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2024-09-14 15:56:53 +0100

    Fix compiler warnings

--- src/config.rs
+++ src/config.rs
@@ -14,11 +14,13 @@ fn expand_home(dir: &str) -> PathBuf {
 /// Configuration values for the program.
 #[derive(Debug)]
 pub struct Config {
+    /// Root location of game files (i.e. `{data_mines}/DataLocal/stage.csv`
+    /// contains the energy cost of each EoC stage).
     pub data_mines: PathBuf,
 }
 
 /// Static variable representing the config.
-// TODO read a file instead of writing it here.
+// TODO read a config file instead of writing it here.
 pub static CONFIG: LazyLock<Config> = LazyLock::new(|| Config {
     data_mines: expand_home("~/Downloads/Version 13.6.0 EN"),
 });
--- src/file_handler.rs
+++ src/file_handler.rs
@@ -1,7 +1,8 @@
 //! Contains functions to read data files.
-use std::{fs, path::{Path, PathBuf}};
+use std::fs;
 use crate::config::CONFIG;
 
+/// temp function
 pub fn do_stuff() {
     let file_name = "DataLocal/stage.csv";
     let content = fs::read_to_string(CONFIG.data_mines.join(file_name)).expect("File name no existo!");
