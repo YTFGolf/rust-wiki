ad4fe6c48c6f9594192248a7c9d760fdafe268a4
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2024-09-21 21:39:56 +0100

    Add file handling to wiki files module

--- src/wiki_files.rs
+++ src/wiki_files.rs
@@ -1,19 +1,76 @@
-use crate::config::CONFIG;
+//! Module that deals with getting and updating wiki files.
+
+use crate::{
+    config::CONFIG,
+    file_handler::{get_file_location, FileLocation},
+};
 use http::header::USER_AGENT;
+use std::{fs::File, io::Read};
 const WIKI_URL: &str = "https://battlecats.miraheze.org/wiki";
+
+/// (file name, wiki page name)
+const FILES: [(&str, &str); 7] = [
+    ("StageNames.csv", "User:TheWWRNerdGuy/data/StageNames.csv"),
+    (
+        "EnemyLinkData.csv",
+        "User:TheWWRNerdGuy/data/EnemyLinkData.csv",
+    ),
+    ("Treasures.csv", "User:TheWWRNerdGuy/data/Treasures.csv"),
+    ("Difficulty.txt", "User:Novastrala/Difficulty.txt"),
+    ("UnitNames.csv", "Module:Cats/names.csv"),
+    ("EnemyNames.csv", "Module:Enemies.csv"),
+    (
+        "ContinueStages.csv",
+        "User:TheWWRNerdGuy/data/ContinueStages.csv",
+    ),
+];
+
+fn get_file_diff(old_content: String, new_content: String) -> String {
+    // let a = text_diff::diff(&old_content, &new_content, "\n");
+    // text_diff::print_diff(&old_content, &new_content, "\n");
+    // for b in a.1.iter(){
+    //     println!("{:?}", b.)
+    // }
+    // println!("{}", );
+    todo!()
+}
 
 ///
 pub fn update_wiki_files() -> Result<(), ureq::Error> {
-    let test_page = "User:TheWWRNerdGuy/Sandbox";
+    let directory = get_file_location(FileLocation::WikiData);
+    std::fs::create_dir_all(directory).unwrap();
+
     let user_agent = format!("{}/rust-wiki-reader", CONFIG.user_name);
-    let uri = format!("{WIKI_URL}/{test_page}?action=raw");
+    for (file_name, page_name) in FILES {
+        let uri = format!("{WIKI_URL}/{page_name}?action=raw");
+        let response = ureq::get(&uri)
+            .set(USER_AGENT.as_str(), &user_agent)
+            .call()?;
+        let content = response.into_string()?;
 
-    let response = ureq::get(&uri)
-        .set(USER_AGENT.as_str(), &user_agent)
-        .call()?;
+        let path = directory.join(file_name);
+        let file = File::options().read(true).open(&path);
 
-    let content = response.into_string()?;
+        let diff: String = match file {
+            Ok(mut f) => {
+                let mut buf = String::new();
+                f.read_to_string(&mut buf).unwrap();
+                get_file_diff(buf, content)
+            }
+            Err(e) => match e.kind() {
+                std::io::ErrorKind::NotFound => "File created".to_string(),
+                _ => panic!("Error when trying to open file: {e}"),
+            },
+        };
 
-    println!("{content:?}");
+        println!("{:#>80}\n{file_name}", '#');
+        println!("{diff}");
+        let x = File::options().write(true).create(true).open(&path);
+        panic!("{x:?}")
+    }
+
     Ok(())
 }
+
+// let mut out = File::create("test").expect("failed to create file");
+// io::copy(&mut resp, &mut out).expect("failed to copy content");
