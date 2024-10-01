4c0583a648bb9dba7c41872bc4541e280eb013e4
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2024-10-01 01:41:35 +0100

    Fix logic error in if statement

--- src/wikitext/stage_info/internal/restrictions.rs
+++ src/wikitext/stage_info/internal/restrictions.rs
@@ -1,5 +1,5 @@
 use crate::{
-    data::stage::parsed::stage::{Restriction, RestrictionCrowns, Stage},
+    data::stage::parsed::stage::{Restriction, RestrictionCrowns as Crowns, Stage},
     wikitext::template_parameter::TemplateParameter,
 };
 use std::num::{NonZero, NonZeroU8};
@@ -11,7 +11,7 @@ const fn non_zero_u8(value: u8) -> NonZero<u8> {
     }
 }
 const FOUR_CROWN_DEFAULT_RESTRICTION: Restriction = Restriction {
-    crowns_applied: RestrictionCrowns::One(non_zero_u8(4)),
+    crowns_applied: Crowns::One(non_zero_u8(4)),
     rarity: NonZeroU8::new(0b000110),
     deploy_limit: None,
     rows: None,
@@ -21,6 +21,7 @@ const FOUR_CROWN_DEFAULT_RESTRICTION: Restriction = Restriction {
 };
 
 fn get_restriction_list(restriction: &Restriction) -> Vec<Vec<u8>> {
+    let _ = restriction;
     todo!()
 }
 
@@ -34,12 +35,17 @@ pub fn restrictions_info(stage: &Stage) -> Option<TemplateParameter> {
         }
 
         let restriction = &restrictions[0];
-        const VALID_ONE_CROWN: [RestrictionCrowns; 2] = [
-            RestrictionCrowns::One(non_zero_u8(1)),
-            RestrictionCrowns::All,
-        ];
-        if stage.crown_data.as_ref().unwrap().max_difficulty > non_zero_u8(1)
-            || VALID_ONE_CROWN.contains(&restriction.crowns_applied)
+        if restriction.crowns_applied != Crowns::All
+            && (stage.crown_data.as_ref().unwrap().max_difficulty > non_zero_u8(1)
+                || restriction.crowns_applied != Crowns::One(non_zero_u8(1)))
+        // invalidate if either:
+        // - stage has multiple crowns available and the restriction does not
+        //   apply to all crowns
+        // - stage has only 1 crown available and the restriction does not apply
+        //   to that crown (i.e. crowns isn't equal to All and isn't equal to
+        //   One(1))
+        // I don't expect any stage to have these problems but it's better to be
+        // safe than sorry (the Rust way!).
         {
             panic!("Unexpected crown error in stage: {stage:?}");
         }
@@ -50,7 +56,13 @@ pub fn restrictions_info(stage: &Stage) -> Option<TemplateParameter> {
         ));
     }
 
-    assert_eq!(restrictions.len(), 2, "Restrictions length is more than 2! (stage: {stage:?})");
+    assert_eq!(
+        restrictions.len(),
+        2,
+        "Restrictions length is more than 2! (stage: {stage:?})"
+    );
+    // There may be cases where restrictions len > 2, but this relies on PONOS
+    // being bad at their jobs so I'll deal with it when it comes up.
 
     todo!()
 }
@@ -71,6 +83,7 @@ mod tests {
         let realm_of_carnage = Stage::new("s 117 0").unwrap();
         assert_eq!(realm_of_carnage.restrictions, None);
         assert_eq!(restrictions_info(&realm_of_carnage), None);
+        assert_eq!(restrictions_section(&realm_of_carnage), vec![]);
     }
 
     #[test]
@@ -81,5 +94,18 @@ mod tests {
             &[FOUR_CROWN_DEFAULT_RESTRICTION]
         );
         assert_eq!(restrictions_info(&earthshaker), None);
+        assert_eq!(restrictions_section(&earthshaker), vec![]);
+    }
+
+    #[test]
+    fn restriction_only_cat() {
+        let finale = Stage::new("c 209 0").unwrap();
+        assert_eq!(
+            restrictions_info(&finale),
+            Some(TemplateParameter::new(
+                b"restriction",
+                b"Unit Restriction: Only [[Cat (Normal Cat)|Cat]]".to_vec()
+            ))
+        )
     }
 }
