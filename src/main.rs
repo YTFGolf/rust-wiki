81196d1509a6c51fe54a12ae26de096e708d2688
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2024-10-18 00:29:02 +0100

    Sort out commands into own functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{Parser, Subcommand};
+use clap::{Args, Parser, Subcommand};
 use rust_wiki::{
     data::stage::parsed::stage::Stage, wiki_files::update_wiki_files,
     wikitext::stage_info::get_stage_info,
@@ -9,14 +9,19 @@ use std::io::{self, Write};
 #[command(version, about, long_about = None)]
 struct Cli {
     #[command(subcommand)]
-    command: Commands,
+    command: Command,
+}
+
+#[derive(Debug, Args)]
+struct StageInfo {
+    selector: Option<String>,
 }
 
 #[derive(Debug, Subcommand)]
-enum Commands {
+enum Command {
     #[command(visible_aliases(["stage"]))]
     /// Get information about a stage.
-    StageInfo,
+    StageInfo(StageInfo),
 
     #[command(visible_aliases(["wiki", "get"]))]
     /// Get data from the wiki.
@@ -26,29 +31,26 @@ enum Commands {
 /*
 TODO
 
-    if cli.command.is_none() {
-        panic!("Command line args not found!")
-    }
-
-- Allow command to be entered via input
-- Properly sort out the different commands into their own functions
 - Allow selector to either be entered via input or via cmd args
 - Add user-config.toml and move config to there
 - Allow cmd options to override user config options
 */
 
-fn main() {
-    let cli = Cli::parse();
-
-    match cli.command {
-        Commands::ReadWiki => update_wiki_files(),
-        _ => (),
-    }
-
+fn stage_info(a: StageInfo) {
+    println!("{a:?}");
     print!("Input file selector: ");
     io::stdout().flush().unwrap();
     let selector = io::stdin().lines().next().unwrap().unwrap();
     println!("{selector:?}");
 
     println!("{}", get_stage_info(&Stage::new(&selector).unwrap()))
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Command::ReadWiki => update_wiki_files(),
+        Command::StageInfo(si) => stage_info(si),
+    }
 }
