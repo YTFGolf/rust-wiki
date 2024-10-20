c8e8e21760df92fbc8271bddba354b43df9a0c01
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2024-10-20 14:16:03 +0100

    Fix doc links to old structs

--- src/data/map/map_option.rs
+++ src/data/map/map_option.rs
@@ -11,7 +11,9 @@ pub struct MapOptionCSV {
     ///
     /// Roughly follows the format of `str(type_id * 1000 + map_id)`, except for
     /// CotC Zombie Outbreaks where it's `str(22000 + map_id)`. Also used in
-    /// [stage_option][crate::data::stage::stage_option::StageOptionCSV::map_id].
+    /// [stage_option][map_id].
+    ///
+    /// [map_id]: crate::data::stage::raw::stage_option::StageOptionCSV::map_id
     pub map_id: u32,
     /// Highest crown difficulty the stage goes up to.
     pub max_difficulty: NonZero<u8>,
--- src/data/stage/parsed.rs
+++ src/data/stage/parsed.rs
@@ -1,4 +1,4 @@
-//! Parses the [StageData][super::stage_data::StageData] object into a more
+//! Parses the [StageData][super::raw::stage_data::StageData] object into a more
 //! accurate representation.
 pub mod stage;
 pub mod stage_enemy;
