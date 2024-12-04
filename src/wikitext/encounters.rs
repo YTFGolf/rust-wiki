796ed04eb553521938e7297b5ae879f5d603643b
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2024-12-04 23:15:47 +0000

    Move section code to submodule

--- src/wikitext/encounters.rs
+++ src/wikitext/encounters.rs
@@ -1,5 +1,8 @@
 //! Write out the encounters of an enemy.
 
+pub mod section;
+use section::SECTIONS;
+
 use crate::{
     config::Config,
     data::{
@@ -54,66 +57,6 @@ fn sort_encounters(encounters: Vec<StageData>) -> Vec<StageData<'_>> {
     encounters.sort_by(|s, o| enumerate_meta(&s.meta).cmp(&enumerate_meta(&o.meta)));
     encounters
 }
-
-#[derive(Debug)]
-/// How you display the section.
-enum DisplayType {
-    /// E.g. EoC: `*Stage x: name (mags)`.
-    Flat,
-    /// Standard `*map: stage` or `*map:\n**stage 1`.
-    Normal,
-    /// Main chapters; require extra logic.
-    Custom,
-    /// Format like Normal but give a warning to the user.
-    Warn,
-    /// Don't parse this at all.
-    Skip,
-}
-type D = DisplayType;
-
-#[derive(Debug)]
-#[allow(dead_code)]
-/// Section of unit encounters.
-pub struct EncountersSection {
-    heading: &'static str,
-    display_type: DisplayType,
-}
-
-const fn get_new_section(heading: &'static str, display_type: DisplayType) -> EncountersSection {
-    EncountersSection {
-        heading,
-        display_type,
-    }
-}
-
-/// Removed stages. No point in being in [SECTIONS] because you need the stage
-/// name for it.
-pub const REMOVED_STAGES: EncountersSection =
-    get_new_section("[[:Category:Removed Content|Removed Stages]]", D::Normal);
-#[rustfmt::skip]
-/// Available sections.
-pub static SECTIONS: [EncountersSection; 17] = [
-    get_new_section("[[Empire of Cats]]",                                    D::Custom),
-    get_new_section("[[Empire of Cats]] [[Zombie Outbreaks|Outbreaks]]",     D::Custom),
-    get_new_section("[[Into the Future]]",                                   D::Custom),
-    get_new_section("[[Into the Future]] [[Zombie Outbreaks|Outbreaks]]",    D::Custom),
-    get_new_section("[[Cats of the Cosmos]]",                                D::Custom),
-    get_new_section("[[Cats of the Cosmos]] [[Zombie Outbreaks|Outbreaks]]", D::Custom),
-    get_new_section("[[The Aku Realms]]",                                    D::Custom),
-
-    get_new_section("[[Legend Stages#Stories of Legend|Stories of Legend]]", D::Flat),
-    get_new_section("[[Legend Stages#Uncanny Legends|Uncanny Legends]]",     D::Flat),
-    get_new_section("[[Legend Stages#Zero Legends|Zero Legends]]",           D::Flat),
-
-    get_new_section("[[Special Events|Event Stages]]",                       D::Normal),
-    get_new_section("[[Underground Labyrinth]]",                             D::Flat),
-    get_new_section("[[Collaboration Event Stages|Collaboration Stages]]",   D::Normal),
-    get_new_section("[[Enigma Stages]]",                                     D::Normal),
-    get_new_section("[[Catclaw Dojo]]",                                      D::Normal),
-
-    get_new_section("Extra Stages",                                          D::Warn),
-    get_new_section("[[Catamin Stages]]",                                    D::Skip),
-];
 
 /// temp
 pub fn do_thing(wiki_id: u32, config: &Config) {
