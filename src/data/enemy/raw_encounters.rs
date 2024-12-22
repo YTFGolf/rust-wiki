24ea7c7b2fa88078fcb2af7827f1ade2fc5e7fe8
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2024-12-22 17:46:09 +0000

    Add stage data iterator function

--- src/data/enemy/raw_encounters.rs
+++ src/data/enemy/raw_encounters.rs
@@ -1,7 +1,9 @@
 //! Deals with enemy encounters.
 
-use crate::data::{stage::raw::stage_data::StageData, version::Version};
-use regex::Regex;
+use crate::data::{
+    stage::{get_stages, raw::stage_data::StageData},
+    version::Version,
+};
 
 /// Does the stage contain the given enemy.
 pub fn stage_contains_enemy(abs_enemy_id: u32, stage: &StageData) -> bool {
@@ -16,19 +18,8 @@ pub fn stage_contains_enemy(abs_enemy_id: u32, stage: &StageData) -> bool {
 ///
 /// `abs_enemy_id` means num in game files, i.e. Doge = 2.
 pub fn get_encounters(abs_enemy_id: u32, version: &Version) -> Vec<StageData<'_>> {
-    let stage_file_re = Regex::new(r"^stage.*?\d{2}\.csv$").unwrap();
-    let dir = &version.get_file_path("DataLocal");
-
-    let files = std::fs::read_dir(dir).unwrap();
-    let encounters = files.filter_map(|f| {
-        let file_name = f.unwrap().file_name().into_string().unwrap();
-        if !stage_file_re.is_match(&file_name) {
-            return None;
-        };
-
-        let stage = StageData::new(&file_name, version).unwrap();
-        stage_contains_enemy(abs_enemy_id, &stage).then_some(stage)
-    });
+    let encounters =
+        get_stages(version).filter(|stage_data| stage_contains_enemy(abs_enemy_id, &stage_data));
 
     encounters.collect()
 }
--- src/data/stage/mod.rs
+++ src/data/stage/mod.rs
@@ -1,3 +1,26 @@
 //! Module that deals with getting information about a stage.
+
 pub mod parsed;
 pub mod raw;
+use super::version::Version;
+use raw::stage_data::StageData;
+use regex::Regex;
+
+/// Get an iterator over all stages in the version.
+pub fn get_stages(version: &Version) -> impl Iterator<Item = StageData<'_>> {
+    let stage_file_re = Regex::new(r"^stage.*?\d{2}\.csv$").unwrap();
+    let dir = &version.get_file_path("DataLocal");
+
+    let files = std::fs::read_dir(dir).unwrap();
+    let stages = files.filter_map(move |f| {
+        let file_name = f.unwrap().file_name().into_string().unwrap();
+        if !stage_file_re.is_match(&file_name) {
+            return None;
+        };
+
+        let stage = StageData::new(&file_name, version).unwrap();
+        Some(stage)
+    });
+
+    stages
+}
