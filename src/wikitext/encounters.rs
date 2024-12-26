715345359bf34132a2e8daaf1caf634092386e2e
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2024-12-26 14:18:02 +0000

    Remove old comments

--- src/wikitext/encounters.rs
+++ src/wikitext/encounters.rs
@@ -496,36 +496,15 @@ pub fn do_thing(wiki_id: u32, config: &Config) {
     */
 }
 
-// Encounter name filter or something
-// Remove all catamin stages
-// move removed to section
-// eliminate unlinked stages and warn
-// move extra stages into correct section
-// remove princess punt eoc stages
-// from stage meta get heading
-// removed is done just by string search
-
 /*
 Due to how the encounters section is constantly evolving, this module cannot be
 tested any other way than empirically.
 
 # Flow
 ## Wikitext
-- Order stages + sort out extra stages
-    - Order is done by a Rust sort
-    - Extra stages will be done with... something idk. Setting to 999 should work
-      since if a stage is an earlier continuation then it would just appear before
-      the later ones.
-- Loop through sections:
-    - Get stage names for each stage
-    - Display stage names. Filter out if doesn't begin with '['.
-        - Hashmap for map name display type
-        - Vec for stage display type
-- If Catamin or extra stages then should print dire warning
-- Else copy to clipboard, message saying "copied to clipboard" in green
+- Copy to clipboard, message saying "copied to clipboard" in green
 
 Other things:
-- StageData::new; StageEnemy::get_magnification
 - Some logging crate needed to log out which pages are skipped
 - Testing can be done easily for small parts but the overall thing can only be
   measured empirically
@@ -534,6 +513,7 @@ Other things:
 /* Testing
 - dedup
 - always appeared at
+  - Include with whitespace in only encounters
 - Removed
 - Extra
 */
