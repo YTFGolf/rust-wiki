85bf7a46d4be98fd5f9d58b76ce484608be5e911
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2024-12-28 17:37:44 +0000

    Add CacheableVersionData notes

--- src/data/version/version_data.rs
+++ src/data/version/version_data.rs
@@ -11,3 +11,12 @@ pub trait CacheableVersionData: Debug + Send + Sync {
     /// Initialises the version data.
     fn init_data(path: &Path) -> Self;
 }
+
+/*
+I don't think this is necessary but I want it in git history.
+///
+/// Safety: This trait is unsafe due to implementation details of
+/// [Version][super::Version] requiring that any struct implementing this trait
+/// is immutable.
+// pub unsafe trait CacheableVersionData: Debug + Send + Sync {
+*/
