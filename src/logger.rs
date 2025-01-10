3c93fcf7c7ecf9517f0b001ec87d5d3fc880479c
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2025-01-10 23:57:24 +0000

    Add basic logger methods

--- src/logger.rs
+++ src/logger.rs
@@ -1,24 +1,63 @@
-use log::{Level, LevelFilter, Metadata, Record, SetLoggerError};
+//! Establishes a logger for the module.
 
-static LOGGER: SimpleExampleLogger = SimpleExampleLogger;
+use std::fmt::Display;
 
-pub fn init_logger() {
-    log::set_logger(&LOGGER).unwrap();
-    log::set_max_level(LevelFilter::Info);
+use log::{Level, LevelFilter, Metadata, Record};
+
+enum Color {
+    Red,
+    Blank,
+}
+impl Color {
+    const fn get_color_num(&self) -> &str {
+        match self {
+            Color::Red => "31",
+            Color::Blank => "0",
+        }
+    }
+}
+// this could be a struct with an enum field
+impl Display for Color {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("\x1b[")?;
+        f.write_str(self.get_color_num())?;
+        f.write_str("m")
+    }
 }
 
 struct SimpleExampleLogger;
-
 impl log::Log for SimpleExampleLogger {
     fn enabled(&self, metadata: &Metadata) -> bool {
         metadata.level() <= Level::Info
     }
 
     fn log(&self, record: &Record) {
-        if self.enabled(record.metadata()) {
-            println!("{} - {}", record.level(), record.args());
+        if !self.enabled(record.metadata()) {
+            return;
         }
+
+        match record.level() {
+            Level::Error => eprintln!(
+                "{red}Error{blank}: {args}",
+                red = Color::Red,
+                blank = Color::Blank,
+                args = record.args()
+            ),
+            Level::Warn => todo!(),
+            Level::Info => todo!(),
+            Level::Debug => todo!(),
+            Level::Trace => todo!(),
+        }
+        println!("{} - {}", record.level(), record.args());
     }
 
     fn flush(&self) {}
+}
+
+static LOGGER: SimpleExampleLogger = SimpleExampleLogger;
+
+/// Initialise the logger. Must be called before logger is used.
+pub fn init_logger() {
+    log::set_logger(&LOGGER).unwrap();
+    log::set_max_level(LevelFilter::Info);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use clap::Parser;
-use log::error;
 use rust_wiki::{
     cli::{
         commands::{Cli, Command},
         parse::{merge_config_and_args, stage_info, update_config},
         user_config::{UserConfig, UserConfigCli},
-    }, config::{get_user_config, Config}, logger::init_logger, wiki_files::update_wiki_files, wikitext::data_files::enemy_data::ENEMY_DATA
+    },
+    config::{get_user_config, Config},
+    logger::init_logger,
+    wiki_files::update_wiki_files,
+    wikitext::data_files::enemy_data::ENEMY_DATA,
 };
 
 fn get_config(config: Option<UserConfig>, args: UserConfigCli) -> Config {
@@ -18,7 +21,7 @@ fn main() {
     let config = get_user_config();
 
     init_logger();
-    error!("epic");
+    log::error!("epic");
 
     match cli.command {
         Command::ReadWiki(c) => update_wiki_files(&get_config(config, c)),
