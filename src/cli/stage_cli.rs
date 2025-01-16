d752a50628e62f7532a680c4420d9282e527e7a4
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2025-01-16 13:51:38 +0000

    Add placeholder stage info command

--- src/cli/stage_cli.rs
+++ src/cli/stage_cli.rs
@@ -3,7 +3,11 @@
 //         stage_info(si, config);
 //     }
 
-use super::base::BaseOptions;
+use super::{
+    base::BaseOptions,
+    cli2::{CliCommand, CommandExec, ConfigMerge},
+};
+use crate::config2::config2::Config;
 use clap::Args;
 
 #[derive(Debug, Args, PartialEq)]
@@ -12,6 +16,27 @@ pub struct StageInfoOptions {
     /// Stage selector.
     pub selector: Vec<String>,
 
+    #[arg(long)]
+    /// Do you put `|0` in the Magnification template instead of the actual
+    /// magnification for gauntlets?
+    pub suppress: Option<bool>,
+
     #[command(flatten)]
+    /// Global options.
     pub base: BaseOptions,
+}
+impl ConfigMerge for StageInfoOptions {
+    fn merge(&self, config: &mut Config) {
+        self.base.merge(config);
+
+        let info = &mut config.stage_info;
+        if let Some(suppress) = self.suppress {
+            info.set_suppress(suppress);
+        }
+    }
+}
+impl CommandExec for StageInfoOptions {
+    fn exec(&self, config: &Config) {
+        todo!()
+    }
 }
