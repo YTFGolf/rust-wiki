ddfc3898087402289035364d35c676cfde2f72d2
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2025-01-16 13:50:46 +0000

    Add cli command traits

--- src/cli/cli2.rs
+++ src/cli/cli2.rs
@@ -7,9 +7,26 @@ pub trait ConfigMerge {
     /// Overwrites values of `config` with equivalent values from `&self`.
     fn merge(&self, config: &mut Config);
 
-    /// Returnable implementation of [ConfigMerge::merge].
-    fn combine(&self, mut config: Config) -> Config {
+    // /// Returnable implementation of [ConfigMerge::merge].
+    // fn combine(&self, mut config: Config) -> Config {
+    //     self.merge(&mut config);
+    //     config
+    // }
+}
+
+/// Execute a CLI command.
+pub trait CommandExec {
+    /// Execute the command.
+    fn exec(&self, config: &Config);
+}
+
+/// Run the CLI command with given options and config.
+pub trait CliCommand: ConfigMerge + CommandExec {
+    ///
+    fn run(&self, mut config: Config) {
         self.merge(&mut config);
-        config
+        self.exec(&config);
     }
 }
+
+impl<T> CliCommand for T where T: ConfigMerge + CommandExec {}
