a49228a30e0d4146be0ccfd3af3ab81ef6626bc1
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2025-01-16 13:15:07 +0000

    Add proper parsing for base options

--- src/cli/base.rs
+++ src/cli/base.rs
@@ -1,8 +1,37 @@
-use clap::Args;
+use super::cli2::ConfigMerge;
+use crate::config2::config2::Config;
+use clap::{
+    builder::{PossibleValue, PossibleValuesParser},
+    Args,
+};
+use log::Level;
+
+/// Get possible values parser for log level.
+///
+/// Do not use this more than necessary, as it leaks memory.
+fn get_levels_values() -> PossibleValuesParser {
+    let a: Vec<_> = log::Level::iter()
+        .map(|v| {
+            let b: &str = v.as_str().to_lowercase().leak();
+            // Could potentially enable clap's `string` feature so this doesn't
+            // need to leak.
+            PossibleValue::new(b)
+        })
+        .collect();
+    PossibleValuesParser::new(a)
+}
 
 #[derive(Debug, Args, PartialEq)]
-/// Base options.
+/// Options that can apply to every submodule.
 pub struct BaseOptions {
-    #[arg(short)]
-    log: Option<String>,
+    #[arg(value_parser = get_levels_values(), short)]
+    /// Log level.
+    pub log: Option<Level>,
+}
+impl ConfigMerge for BaseOptions {
+    fn merge(&self, config: &mut Config) {
+        if let Some(log) = self.log {
+            config.log_level = log;
+        }
+    }
 }
