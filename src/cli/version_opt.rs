6d07b8177a102c31f2b51b35ff0f5b0b2de02d89
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2025-01-16 18:20:00 +0000

    Remove unnecessary code

--- src/cli/version_opt.rs
+++ src/cli/version_opt.rs
@@ -1,12 +1,6 @@
 use super::cli::ConfigMerge;
 use crate::config2::config2::Config;
-use clap::{builder::PossibleValuesParser, Args};
-use log::Level;
-
-const POSSIBLE_LOG_LEVELS: [&str; 5] = ["error", "warn", "info", "debug", "trace"];
-fn get_levels_values() -> PossibleValuesParser {
-    PossibleValuesParser::new(POSSIBLE_LOG_LEVELS)
-}
+use clap::Args;
 
 #[derive(Debug, Args, PartialEq)]
 /// Options that can apply to every submodule.
