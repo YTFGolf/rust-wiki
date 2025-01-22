b72bf9599513b538462f43fb90ddb98336353baf
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2025-01-22 16:53:12 +0000

    Add base to wiki cli

--- src/cli/wiki_cli.rs
+++ src/cli/wiki_cli.rs
@@ -1,6 +1,9 @@
 //! `read_wiki` command.
 
-use super::cli_util::{CommandExec, ConfigMerge};
+use super::{
+    base::BaseOptions,
+    cli_util::{CommandExec, ConfigMerge},
+};
 use crate::{config::Config, wiki_files::update_wiki_files};
 use clap::Args;
 
@@ -9,10 +12,15 @@ use clap::Args;
 pub struct ReadWikiOptions {
     /// Wiki username.
     pub username: Option<String>,
+
+    #[command(flatten)]
+    /// Global options.
+    pub base: BaseOptions,
 }
 
 impl ConfigMerge for ReadWikiOptions {
     fn merge(&self, config: &mut Config) {
+        self.base.merge(config);
         if let Some(username) = &self.username {
             config.wiki.username.clone_from(username);
         }
