53691438f38b9a3d97a737f3bc1c2702d2bdb4b5
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2025-01-24 16:54:05 +0000

    Remove unnecessary main code

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,6 @@ use rust_wiki::{
 };
 use std::process::exit;
 
-fn temp() {
-    if true {
-        return;
-    }
-    // use serde::Serialize;
-    use rust_wiki::config::Config;
-    let def_config = Config::default();
-    println!("{def_config:?}");
-    println!("{}", toml::to_string(&def_config).unwrap());
-    println!("{}", serde_json::to_string(&def_config).unwrap());
-
-    let toml_repr = toml::to_string(&def_config).unwrap();
-    println!("{:?}", toml::from_str::<Config>(&toml_repr).unwrap());
-}
-
 fn initialise_config() -> ! {
     println!("Config not found, initialising...");
     Config::initialise();
@@ -30,7 +15,6 @@ fn initialise_config() -> ! {
 }
 
 fn main() {
-    temp();
     let cli = Cli::parse();
     let config: Config =
         toml::from_str(&Config::read_config_file().unwrap_or_else(|| initialise_config())).unwrap();
