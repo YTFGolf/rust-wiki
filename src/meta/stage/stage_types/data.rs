644eaceaa4306e7c9073472e5223ee719b2fe3b1
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2025-03-02 18:11:57 +0000

    Add raw stage types data

--- src/meta/stage/stage_types/data.rs
+++ src/meta/stage/stage_types/data.rs
@@ -26,6 +26,7 @@ use crate::meta::stage::variant::StageVariantID;
 type T = StageVariantID;
 type C = StageCodeType;
 
+/// Initialise raw stage type.
 const fn init(
     name: &'static str,
     map_code: Option<&'static str>,
@@ -42,73 +43,44 @@ const fn init(
     }
 }
 
-const MAX_RAW_TYPES: usize = 1;
+// raw data for stage types
 #[rustfmt::skip]
-const RAW_STAGE_TYPES: [StageType; MAX_RAW_TYPES] = [
-    init("Stories of Legend", Some("N"), C::RPrefix, T::SoL, "SoL"),
+const RAW_STAGE_TYPES: [StageType; 24] = [
+    init("Stories of Legend",            Some("N"),  C::RPrefix,     T::SoL,            "SoL"),
+    init("Event Stages",                 Some("S"),  C::RPrefix,     T::Event,          "Event|Special"),
+    init("Collaboration Stages",         Some("C"),  C::RPrefix,     T::Collab,         "Collab"),
+    init("Main Chapters",                None,       C::Custom,      T::MainChapters,   "main|EoC|ItF|W|CotC|Space"),
+    init("Extra Stages",                 Some("RE"), C::Other("EX"), T::Extra,          "Extra"),
+    init("Catclaw Dojo",                 Some("T"),  C::RPrefix,     T::Dojo,           "Dojo"),
+    init("Towers",                       Some("V"),  C::RPrefix,     T::Tower,          "Tower"),
+    init("Ranking Dojo",                 Some("R"),  C::RPrefix,     T::RankingDojo,    "Rank"),
+    init("Challenge Battle",             Some("M"),  C::RPrefix,     T::Challenge,      "Challenge"),
+
+    init("Uncanny Legends",              Some("NA"), C::RPrefix,     T::UL,             "UL"),
+    init("Catamin Stages",               Some("B"),  C::RPrefix,     T::Catamin,        "Catamin"),
+    // init("Legend Quest",                 Some("D"),  C::Map,         T::LegendQuest,    "Haha"),
+    init("Empire of Cats Outbreaks",     None,       C::Custom,      T::EocOutbreak,    "Z 1|Z 2|Z 3"),
+    init("Into the Future Outbreaks",    None,       C::Custom,      T::EocOutbreak,    "Z 4|Z 5|Z 6"),
+    init("Cats of the Cosmos Outbreaks", None,       C::Custom,      T::EocOutbreak,    "Z 7|Z 8|Z 9"),
+    init("Filibuster Invasion",          None,       C::Custom,      T::Filibuster,     "Filibuster"),
+    init("Gauntlets",                    Some("A"),  C::RPrefix,     T::Gauntlet,       "Gauntlet|Baron"),
+    init("Enigma Stages",                Some("H"),  C::RPrefix,     T::Enigma,         "Enigma"),
+    init("Collab Gauntlets",             Some("CA"), C::RPrefix,     T::CollabGauntlet, ""),
+
+    init("Aku Realms",                   Some("DM"), C::Map,         T::AkuRealms,      "Aku"),
+    init("Behemoth Culling",             Some("Q"),  C::RPrefix,     T::Behemoth,       "Behemoth"),
+    init("Labyrinth",                    Some("L"),  C::Map,         T::Labyrinth,      "Labyrinth"),
+    init("Zero Legends",                 Some("ND"), C::RPrefix,     T::ZL,             "ZL"),
+    init("Colosseum",                    Some("SR"), C::RPrefix,     T::Colosseum,      "Colosseum"),
+    init("Catclaw Championships",        Some("G"),  C::Map,         T::Championships,  "Championships"),
 ];
-// pub const LEGACY_STAGE_TYPES: [LegacyStageType; 22] = [
-//     initialise_stage_type("Stories of Legend",            000, "N",     true,  T::SoL),
-//     initialise_stage_type("Event Stages",                 001, "S",     true,  T::Event),
-//     initialise_stage_type("Collaboration Stages",         002, "C",     true,  T::Collab),
-//     initialise_stage_type("Main Chapters",                003, "main",  false, T::MainChapters),
-//     initialise_stage_type("Extra Stages",                 004, "RE|EX", false, T::Extra),
-//     initialise_stage_type("Catclaw Dojo",                 006, "T",     true,  T::Dojo),
-//     initialise_stage_type("Towers",                       007, "V",     true,  T::Tower),
-//     initialise_stage_type("Ranking Dojo",                 011, "R",     true,  T::RankingDojo),
-//     initialise_stage_type("Challenge Battle",             012, "M",     true,  T::Challenge),
-
-//     initialise_stage_type("Uncanny Legends",              013, "NA",    true,  T::UL),
-//     initialise_stage_type("Catamin Stages",               014, "B",     true,  T::Catamin),
-//     // initialise_stage_type("Empire of Cats Outbreaks",     020, "",      false, T::EocOutbreak),
-//     // initialise_stage_type("Into the Future Outbreaks",    021, "",      false, T::ItfOutbreak),
-//     // initialise_stage_type("Cats of the Cosmos Outbreaks", 022, "",      false, T::CotcOutbreak),
-//     initialise_stage_type("Outbreaks",                    999, "main",  false, T::Outbreaks),
-
-//     initialise_stage_type("Filibuster Invasion",          023, "",      false, T::Filibuster),
-//     initialise_stage_type("Gauntlets",                    024, "A",     true,  T::Gauntlet),
-//     initialise_stage_type("Enigma Stages",                025, "H",     true,  T::Enigma),
-//     initialise_stage_type("Collab Gauntlets",             027, "CA",    true,  T::CollabGauntlet),
-
-//     initialise_stage_type("Aku Realms",                   030, "DM",    false, T::AkuRealms),
-//     initialise_stage_type("Behemoth Culling",             031, "Q",     true,  T::Behemoth),
-//     initialise_stage_type("Labyrinth",                    033, "L",     false, T::Labyrinth),
-//     initialise_stage_type("Zero Legends",                 034, "ND",    true,  T::ZL),
-//     initialise_stage_type("Colosseum",                    036, "SR",    true,  T::Colosseum),
-//     initialise_stage_type("Catclaw Championships",        037, "G",     false, T::Championships),
-// ];
-
-// static STAGE_TYPE_MAP: LazyLock<[LegacyStageTypeMap; 22]> = LazyLock::new(|| {[
-//     initialise_type_map("SoL|0|N|RN",               T::SoL),
-//     initialise_type_map("Event|Special|1|S|RS",     T::Event),
-//     initialise_type_map("Collab|2|C|RC",            T::Collab),
-//     initialise_type_map("EoC|ItF|W|CotC|Space",     T::MainChapters),
-//     initialise_type_map("Extra|4|RE|EX",            T::Extra),
-//     initialise_type_map("Dojo|6|T|RT",              T::Dojo),
-//     initialise_type_map("Tower|7|V|RV",             T::Tower),
-//     initialise_type_map("Rank|11|R|RR",             T::RankingDojo),
-//     initialise_type_map("Challenge|12|M|RM",        T::Challenge),
-
-//     initialise_type_map("UL|13|NA|RNA",             T::UL),
-//     initialise_type_map("Catamin|14|B|RB",          T::Catamin),
-//     initialise_type_map("Z",                        T::Outbreaks),
-//     initialise_type_map("Filibuster|23",            T::Filibuster),
-//     // initialise_type_map("LQ|16|D",                  "Why would you want to do Legend Quest"),
-//     initialise_type_map("Gauntlet|Baron|24|A|RA",   T::Gauntlet),
-//     initialise_type_map("Enigma|25|H|RH",           T::Enigma),
-//     initialise_type_map("27|CA|RCA",                T::CollabGauntlet),
-//     initialise_type_map("Aku|30|DM",                T::AkuRealms),
-
-//     initialise_type_map("Behemoth|31|Q|RQ",         T::Behemoth),
-//     initialise_type_map("Labyrinth|33|L",           T::Labyrinth),
-//     initialise_type_map("ZL|34|ND|RND",             T::ZL),
-//     initialise_type_map("Colosseum|36|SR|RSR",      T::Colosseum),
-//     initialise_type_map("Championships|37|G",       T::Championships),
-// ]});
 
 const MAX_VARIANT_NUMBER: usize = 37;
 // store the data, store the map
-const STAGE_TYPES: [Option<StageType>; MAX_VARIANT_NUMBER] = [const { None }; MAX_VARIANT_NUMBER];
+const STAGE_TYPES: [Option<StageType>; MAX_VARIANT_NUMBER] = {
+    let a = [const { None }; MAX_VARIANT_NUMBER];
+    a
+};
 
 const fn variant_to_index(variant: StageVariantID) -> usize {
     variant.num() as usize
@@ -166,6 +138,10 @@ mod tests {
         todo!()
         // check that no duplicate match possibilities exist
     }
+
+    // assert that none always matches with custom
+    // assert all others work the conventional way
+    // assert no duplicates appear in raw types
 }
 
 /*
