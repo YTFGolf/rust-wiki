6d6ba23b768012b87694e32775cd8429a929ec08
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2025-03-07 16:46:53 +0000

    Create custom subset variant for map transform

--- src/meta/stage/stage_types/transform.rs
+++ src/meta/stage/stage_types/transform.rs
@@ -1,14 +1,42 @@
 //! Transform ID data into various formats.
 
+use crate::meta::stage::variant::StageVariantID;
+
 pub mod transform_map;
 pub mod transform_stage;
 
+/// Every [`StageVariantID`] that has a custom [`StageCodeType`].
+enum CustomVariantID {
+    MainChapters,
+    EocOutbreak,
+    ItfOutbreak,
+    CotcOutbreak,
+    Filibuster,
+}
+impl CustomVariantID {
+    fn new(variant: StageVariantID) -> Option<Self> {
+        type T = StageVariantID;
+        let custom = match variant {
+            T::MainChapters => Self::MainChapters,
+            T::EocOutbreak => Self::EocOutbreak,
+            T::ItfOutbreak => Self::ItfOutbreak,
+            T::CotcOutbreak => Self::CotcOutbreak,
+            T::Filibuster => Self::Filibuster,
+            _ => return None,
+        };
+        Some(custom)
+    }
+}
+impl From<StageVariantID> for CustomVariantID {
+    fn from(value: StageVariantID) -> Self {
+        Self::new(value).unwrap()
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::meta::stage::{
-        stage_types::{iter_stage_types, types::StageCodeType},
-        variant::StageVariantID as T,
-    };
+    use super::*;
+    use crate::meta::stage::stage_types::{iter_stage_types, types::StageCodeType};
 
     #[test]
     fn test_custom_stypes() {
@@ -16,14 +44,9 @@ mod tests {
         for stype in iter_stage_types() {
             let data = stype.data;
             if data.map_code.is_none() || data.stage_code == StageCodeType::Custom {
-                assert!(matches!(
-                    data.variant_id,
-                    T::MainChapters
-                        | T::EocOutbreak
-                        | T::ItfOutbreak
-                        | T::CotcOutbreak
-                        | T::Filibuster
-                ));
+                assert!(CustomVariantID::new(data.variant_id).is_some());
+            } else {
+                assert!(CustomVariantID::new(data.variant_id).is_none());
             }
         }
     }
