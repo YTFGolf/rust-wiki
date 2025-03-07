acc8438349cf615c7e0a928837d89ae1af053f90
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2025-03-07 17:09:48 +0000

    Add map data file transformer

--- src/meta/stage/stage_types/transform/transform_map.rs
+++ src/meta/stage/stage_types/transform/transform_map.rs
@@ -1,9 +1,38 @@
 //! Transform [`MapID`] data into various formats.
 
-use crate::meta::stage::map_id::MapID;
-fn _map_data_file(_map_id: MapID) -> String {
-    // if let Some(code) = map_id {
+use super::CustomVariantID as T;
+use crate::meta::stage::{
+    map_id::{MainType, MapID},
+    stage_types::get_stage_type,
+};
 
-    // }
-    todo!()
+/// Get map's data file name.
+pub fn map_data_file(map_id: MapID) -> String {
+    let stype = get_stage_type(map_id.variant()).data;
+    if let Some(code) = stype.map_code {
+        return format!("MapStageData{code}_{num:03}.csv", num = map_id.num());
+    }
+
+    let variant: T = stype.variant_id.into();
+
+    match variant {
+        T::Filibuster => "stageNormal2_2_Invasion.csv".to_string(),
+        T::MainChapters => {
+            let main = map_id
+                .main_type()
+                .unwrap_or_else(|| panic!("Main chapter {n} out of bounds!", n = map_id.num()));
+
+            match main {
+                MainType::EoC => "stageNormal0.csv".to_string(),
+                MainType::ItF => format!("stageNormal1_{num}.csv", num = map_id.num() - 3),
+                MainType::CotC => format!("stageNormal2_{num}.csv", num = map_id.num() - 6),
+            }
+        }
+        T::EocOutbreak | T::ItfOutbreak | T::CotcOutbreak => {
+            let stype = map_id.variant().num() - 20;
+            // 20 = EoC Z = stageNormal0_{stage}_Z.csv
+            let num = map_id.num();
+            format!("stageNormal{stype}_{num}_Z.csv")
+        }
+    }
 }
