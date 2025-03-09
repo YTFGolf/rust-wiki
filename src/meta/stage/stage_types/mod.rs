f4eac5039159a7b0755a3595ec823cb80c8d9e03
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2025-03-09 11:54:11 +0000

    Update stage types doc

--- src/meta/stage/stage_types/mod.rs
+++ src/meta/stage/stage_types/mod.rs
@@ -1,5 +1,4 @@
-//! Defines information that can be used to obtain or transform pure data from
-//! or into usable formats.
+//! Allows you to obtain pure data from, or transform into, usable formats.
 
 // Note: this module is intended to just be fragile spaghetti code that only
 // depends on itself and the ID types in super, and provides an interface that
