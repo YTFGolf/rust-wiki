444dfa6a185e0893063f0023d116224a84853416
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2025-03-21 19:18:37 +0000

    Add notes about required data

--- src/data/cat/parsed.rs
+++ src/data/cat/parsed.rs
@@ -1,13 +1,66 @@
 #![allow(missing_docs, unused_imports, dead_code)]
 
 use super::raw::CombinedCatData;
+use std::rc::Rc;
 
 #[derive(Debug)]
-struct Cat {}
+pub enum Ability {}
+// will need to be done better. Trait or proc macro I think
+/*
+/// Is it removed with curse.
+is_cursable
+/// Does this occur on every hit.
+is_general
+*/
+
+#[derive(Debug)]
+pub enum EnemyType {}
+
+#[derive(Debug)]
+pub struct AttackRange {}
+
+#[derive(Debug)]
+pub struct AttackHit {
+    active_ability: bool,
+    range: AttackRange,
+    foreswing: u32,
+    backswing: u32,
+}
+
+#[derive(Debug)]
+pub enum AttackHits {
+    Single([AttackHit; 1]),
+    Double([AttackHit; 2]),
+    Triple([AttackHit; 3]),
+}
+
+#[derive(Debug)]
+pub struct Attack {
+    abilities: Rc<[Ability]>,
+    targets: Rc<[EnemyType]>,
+    hits: AttackHits,
+}
+
+#[derive(Debug)]
+struct Cat {
+    base_hp: u32,
+    kb: u16,
+    speed: u8,
+    // atk: u32,
+    // tba: u16,
+    // range: u16,
+    price: u16,
+    // width: u16,
+}
 
 impl Cat {
-    fn from_combined(_data: CombinedCatData) -> Self {
-        Self {}
+    fn from_combined(_data: &CombinedCatData) -> Self {
+        Self {
+            base_hp: todo!(),
+            kb: todo!(),
+            speed: todo!(),
+            price: todo!(),
+        }
     }
 }
 
@@ -29,7 +82,7 @@ mod tests {
         panic!(
             "{:#?}",
             read_data_file(file_name, version)
-                .map(Cat::from_combined)
+                .map(|comb| Cat::from_combined(&comb))
                 .collect::<Vec<_>>()
         )
     }
