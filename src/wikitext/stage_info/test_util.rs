4c1e1ca08db0237e8049187ba6df71fe86a41033
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2025-04-04 17:47:05 +0100

    Deprecate test_util

--- src/wikitext/stage_info/test_util.rs
+++ src/wikitext/stage_info/test_util.rs
@@ -5,6 +5,7 @@ use crate::{
     wikitext::{data_files::stage_wiki_data::STAGE_WIKI_DATA, stage_info::StageWikiDataContainer},
 };
 /// Get the stage's [StageWikiData] for a test function.
+#[deprecated]
 pub fn get_stage_wiki_data(stage: &Stage) -> StageWikiDataContainer {
     let stage_map = STAGE_WIKI_DATA.stage_map(stage.id.map()).unwrap();
     let stage_name = stage_map.get(stage.id.num()).unwrap();
