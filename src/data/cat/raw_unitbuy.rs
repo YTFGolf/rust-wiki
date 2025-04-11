959e7fee1ff47b8e897254d64e211730cb77f107
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2025-04-11 20:20:29 +0100

    Better infrastructure for unitbuy

--- src/data/cat/raw_unitbuy.rs
+++ src/data/cat/raw_unitbuy.rs
@@ -8,13 +8,16 @@ pub struct UnitBuy {
 
 fn get_unitbuy(path: &Path) -> Vec<UnitBuy> {
     let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(false)
         .from_path(path.join("DataLocal/unitbuy.csv"))
         .unwrap();
 
     rdr.byte_records()
         .map(|record| {
             let result = record.unwrap();
-            let unit: UnitBuy = result.deserialize(None).unwrap();
+            let unit: UnitBuy = result
+                .deserialize(None)
+                .unwrap_or_else(|e| panic!("Error when parsing record {result:?}: {e}"));
             unit
         })
         .collect()
@@ -31,9 +34,15 @@ mod tests {
         let path = version.get_file_path("");
         let units = get_unitbuy(&path);
 
-        let ids = [0, 25, 177, 543, 658];
-        for id in ids{
+        let ids = [0, 8, 25, 177, 543, 626, 643, 658];
+        for id in ids {
             println!("{:?}", units[id])
+        }
+
+        #[allow(unused)]
+        for (i, unit) in units.iter().enumerate() {
+            // if unit._uk12 != 0 { println!("{i}: {:?}", unit) }
+            // if unit.max_xp_upgrade != 20 { println!("{i}: {:?}", unit.max_xp_upgrade) }
         }
     }
 }
