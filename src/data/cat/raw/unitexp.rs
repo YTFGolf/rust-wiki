9d519470c37a0c2fdacf791cb36cc46ef0b86c6c
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2025-04-11 23:41:40 +0100

    Remove cached data and just use constants

--- src/data/cat/raw/unitexp.rs
+++ src/data/cat/raw/unitexp.rs
@@ -1,6 +1,4 @@
-use crate::data::version::version_data::CacheableVersionData;
-use csv::{ByteRecord, Error};
-use std::{fmt::Debug, path::Path};
+use std::fmt::Debug;
 
 #[derive(Debug, serde::Deserialize, PartialEq, Eq, PartialOrd, Ord)]
 #[allow(missing_docs)]
@@ -27,51 +25,55 @@ pub struct UnitExp {
     until_200: u8,
 }
 
-fn parse_unitexp_error(e: &Error, result: &ByteRecord) -> impl Debug {
-    let index = match e.kind() {
-        csv::ErrorKind::Deserialize { pos: _, err } => err.field().unwrap(),
-        _ => unimplemented!(),
-    };
+const DEFAULT: UnitExp = UnitExp {
+    until_10: 10,
+    until_20: 20,
+    until_30: 30,
+    until_40: 35,
+    until_50: 40,
+    until_60: 45,
+    until_70: 50,
+    until_80: 55,
+    until_90: 60,
+    until_100: 65,
+    until_110: 70,
+    until_120: 75,
+    until_130: 80,
+    until_140: 85,
+    until_150: 90,
+    until_160: 95,
+    until_170: 100,
+    until_180: 105,
+    until_190: 110,
+    until_200: 115,
+};
+const SUPERFELINE: UnitExp = UnitExp {
+    until_10: 10,
+    until_20: 10,
+    until_30: 10,
+    until_40: 10,
+    until_50: 10,
+    until_60: 10,
+    until_70: 10,
+    until_80: 10,
+    until_90: 10,
+    until_100: 10,
+    until_110: 10,
+    until_120: 10,
+    until_130: 10,
+    until_140: 10,
+    until_150: 10,
+    until_160: 10,
+    until_170: 10,
+    until_180: 10,
+    until_190: 10,
+    until_200: 10,
+};
 
-    String::from_utf8(result[index as usize].into()).unwrap()
-}
-
-fn get_unitexp(path: &Path) -> Vec<UnitExp> {
-    let mut rdr = csv::ReaderBuilder::new()
-        .has_headers(false)
-        .flexible(false)
-        .from_path(path.join("DataLocal/unitexp.csv"))
-        .unwrap();
-
-    rdr.byte_records()
-        .map(|record| {
-            let result = record.unwrap();
-            assert_eq!(result.len(), 20);
-            let unit: UnitExp = result.deserialize(None).unwrap_or_else(|e| {
-                panic!(
-                    "Error when parsing record {result:?}: {e}. Item was {item:?}.",
-                    item = parse_unitexp_error(&e, &result)
-                )
-            });
-            unit
-        })
-        .collect()
-}
-
-#[derive(Debug)]
-struct UnitExpContainer {
-    units: Vec<UnitExp>,
-}
-impl UnitExpContainer {
-    pub fn get_unit(&self, id: u32) -> &UnitExp {
-        &self.units[id as usize]
-    }
-}
-impl CacheableVersionData for UnitExpContainer {
-    fn init_data(path: &Path) -> Self {
-        Self {
-            units: get_unitexp(path),
-        }
+pub const fn get_levelling(id: u32) -> UnitExp {
+    match id {
+        643 => SUPERFELINE,
+        _ => DEFAULT,
     }
 }
 
@@ -79,63 +81,32 @@ impl CacheableVersionData for UnitExpContainer {
 mod tests {
     use super::*;
     use crate::config::TEST_CONFIG;
+    use std::path::Path;
 
-    const DEFAULT: UnitExp = UnitExp {
-        until_10: 10,
-        until_20: 20,
-        until_30: 30,
-        until_40: 35,
-        until_50: 40,
-        until_60: 45,
-        until_70: 50,
-        until_80: 55,
-        until_90: 60,
-        until_100: 65,
-        until_110: 70,
-        until_120: 75,
-        until_130: 80,
-        until_140: 85,
-        until_150: 90,
-        until_160: 95,
-        until_170: 100,
-        until_180: 105,
-        until_190: 110,
-        until_200: 115,
-    };
-    const SUPERFELINE: UnitExp = UnitExp {
-        until_10: 10,
-        until_20: 10,
-        until_30: 10,
-        until_40: 10,
-        until_50: 10,
-        until_60: 10,
-        until_70: 10,
-        until_80: 10,
-        until_90: 10,
-        until_100: 10,
-        until_110: 10,
-        until_120: 10,
-        until_130: 10,
-        until_140: 10,
-        until_150: 10,
-        until_160: 10,
-        until_170: 10,
-        until_180: 10,
-        until_190: 10,
-        until_200: 10,
-    };
+    fn get_unitexp(path: &Path) -> Vec<UnitExp> {
+        let mut rdr = csv::ReaderBuilder::new()
+            .has_headers(false)
+            .flexible(false)
+            .from_path(path.join("DataLocal/unitexp.csv"))
+            .unwrap();
+
+        rdr.byte_records()
+            .map(|record| {
+                let result = record.unwrap();
+                assert_eq!(result.len(), std::mem::size_of::<UnitExp>());
+                let unit: UnitExp = result.deserialize(None).unwrap();
+                unit
+            })
+            .collect()
+    }
 
     #[test]
     fn test_file_reader() {
         let version = TEST_CONFIG.version.current_version();
-        let unitexp = version.get_cached_file::<UnitExpContainer>();
-        let units = &unitexp.units;
+        let units = get_unitexp(&version.get_file_path(""));
 
-        for (i, unit) in units.iter().enumerate() {
-            match i {
-                643 => assert_eq!(unit, &SUPERFELINE),
-                _ => assert_eq!(unit, &DEFAULT),
-            }
+        for (i, unit) in units.into_iter().enumerate() {
+            assert_eq!(get_levelling(i as u32), unit);
         }
     }
 
@@ -143,11 +114,11 @@ mod tests {
     fn test_units() {
         // print them out just for show
         let version = TEST_CONFIG.version.current_version();
-        let unitexp = version.get_cached_file::<UnitExpContainer>();
+        let unitexp = get_unitexp(&version.get_file_path(""));
 
         let test_units = [("cat", 0), ("sfeline", 643)];
         for (name, id) in test_units {
-            println!("{name} ({id}) = {:?}", unitexp.get_unit(id));
+            println!("{name} ({id}) = {:?}", unitexp[id]);
         }
     }
 }
