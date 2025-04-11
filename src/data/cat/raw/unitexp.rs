c778c8473bf9cd04d7b3c994a61fb4966554d12c
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2025-04-12 00:08:14 +0100

    More in-depth docs for unitexp

--- src/data/cat/raw/unitexp.rs
+++ src/data/cat/raw/unitexp.rs
@@ -1,28 +1,57 @@
 //! Deals with level-up cost multipliers.
+//!
+//! Because (as of 14.3) Superfeline is the *only* unit with different
+//! multipliers (constant price instead of increasing every 10 levels) this
+//! doesn't use [`CacheableVersionData`][CacheableVersionData] but instead uses
+//! constants. Unit tests check these constants for correctness.
+//!
+//! [CacheableVersionData]: crate::data::version::version_data
 
 #[derive(Debug, serde::Deserialize, PartialEq, Eq, PartialOrd, Ord)]
-/// Level-up cost per 10 levels (multiplied by 10).
+/// Level-up cost multiplier per 10 levels.
+///
+/// All values are multiplied by 10 to avoid using floats.
 pub struct UnitExp {
-    until_10: u8,
-    until_20: u8,
-    until_30: u8,
-    until_40: u8,
-    until_50: u8,
-    until_60: u8,
-    until_70: u8,
-    until_80: u8,
-    until_90: u8,
-    until_100: u8,
-    until_110: u8,
-    until_120: u8,
-    until_130: u8,
-    until_140: u8,
-    until_150: u8,
-    until_160: u8,
-    until_170: u8,
-    until_180: u8,
-    until_190: u8,
-    until_200: u8,
+    /// XP cost multiplier up to level 10.
+    pub until_10: u8,
+    /// XP cost multiplier up to level 20.
+    pub until_20: u8,
+    /// XP cost multiplier up to level 30.
+    pub until_30: u8,
+    /// XP cost multiplier up to level 40.
+    pub until_40: u8,
+    /// XP cost multiplier up to level 50.
+    pub until_50: u8,
+    /// XP cost multiplier up to level 60.
+    pub until_60: u8,
+    /// XP cost multiplier up to level 70.
+    pub until_70: u8,
+    /// XP cost multiplier up to level 80.
+    pub until_80: u8,
+    /// XP cost multiplier up to level 90.
+    pub until_90: u8,
+    /// XP cost multiplier up to level 100.
+    pub until_100: u8,
+    /// XP cost multiplier up to level 110.
+    pub until_110: u8,
+    /// XP cost multiplier up to level 120.
+    pub until_120: u8,
+    /// XP cost multiplier up to level 130.
+    pub until_130: u8,
+    /// XP cost multiplier up to level 140.
+    pub until_140: u8,
+    /// XP cost multiplier up to level 150.
+    pub until_150: u8,
+    /// XP cost multiplier up to level 160.
+    pub until_160: u8,
+    /// XP cost multiplier up to level 170.
+    pub until_170: u8,
+    /// XP cost multiplier up to level 180.
+    pub until_180: u8,
+    /// XP cost multiplier up to level 190.
+    pub until_190: u8,
+    /// XP cost multiplier up to level 200.
+    pub until_200: u8,
 }
 
 /// Multipliers for nearly every enemy in the game.
@@ -97,6 +126,7 @@ mod tests {
             .map(|record| {
                 let result = record.unwrap();
                 assert_eq!(result.len(), std::mem::size_of::<UnitExp>());
+                // make sure that the struct definition is up-to-date
                 let unit: UnitExp = result.deserialize(None).unwrap();
                 unit
             })
