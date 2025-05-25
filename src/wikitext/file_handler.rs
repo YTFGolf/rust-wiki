da79dc96ef82a009875bdc6f226444c1e10a10fe
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2025-05-25 13:24:49 +0100

    Remove useless enum

--- src/wikitext/file_handler.rs
+++ src/wikitext/file_handler.rs
@@ -4,23 +4,7 @@ use std::{path::PathBuf, sync::LazyLock};
 static WIKI_DATA_LOCATION: LazyLock<PathBuf> =
     LazyLock::new(|| std::env::current_dir().unwrap().join("data"));
 
-/// Describes a location for files.
-pub enum FileLocation {
-    /// Root directory of game data.
-    // GameData,
-    /// Root directory of downloaded wiki data.
-    WikiData,
-}
-use FileLocation as F;
-
-/// Get the root directory of a location.
-fn get_file_location(location: &FileLocation) -> &'static PathBuf {
-    match location {
-        F::WikiData => &WIKI_DATA_LOCATION,
-    }
-}
-
 /// Get root directory of wiki data.
 pub fn get_wiki_data_location() -> &'static PathBuf {
-    get_file_location(&FileLocation::WikiData)
+    &WIKI_DATA_LOCATION
 }
