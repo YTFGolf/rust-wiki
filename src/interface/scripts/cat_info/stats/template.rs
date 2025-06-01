16c0ea43ed4c3f59cc19f0ddc576bd8cee16318f
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2025-06-01 19:14:35 +0100

    Add form struct

--- src/interface/scripts/cat_info/stats/template.rs
+++ src/interface/scripts/cat_info/stats/template.rs
@@ -11,10 +11,22 @@ use crate::{
 use num_format::{Locale, ToFormattedString};
 use std::cmp::max;
 
+struct Form {
+    range: String,
+    attack_cycle: String,
+    speed: String,
+    knockback: String,
+    animation: String,
+    recharge: String,
+    hp_max: String,
+    atk_max: String,
+    attack_type: &'static str,
+    abilities: String,
+}
+
 pub fn get_template(cat: Cat) {
-    let forms = &cat.forms;
-    let stats = &forms.stats[0];
-    let anims = &forms.anims[0];
+    let stats = &cat.forms.stats[0];
+    let anims = &cat.forms.anims[0];
 
     let level = 30;
     let foreswing = stats.attack.hits.foreswing();
@@ -112,6 +124,21 @@ pub fn get_template(cat: Cat) {
         abilities.join("<br>\n")
     };
 
+    // template
+
+    let form = Form {
+        range,
+        attack_cycle,
+        speed,
+        knockback,
+        animation,
+        recharge,
+        hp_max,
+        atk_max,
+        attack_type,
+        abilities,
+    };
+
     let mut t = Template::named("Cat Stats");
     type P = TemplateParameter;
 
@@ -119,16 +146,16 @@ pub fn get_template(cat: Cat) {
     t.push_params(P::new("Hp Normal", base_hp));
     t.push_params(P::new("Atk Power Normal", base_atk));
 
-    t.push_params(P::new("Atk Range Normal", range));
-    t.push_params(P::new("Attack Frequency Normal", attack_cycle));
-    t.push_params(P::new("Movement Speed Normal", speed));
-    t.push_params(P::new("Knockback Normal", knockback));
-    t.push_params(P::new("Attack Animation Normal", animation));
-    t.push_params(P::new("Recharging Time Normal", recharge));
-    t.push_params(P::new("Hp Normal Lv.MAX", hp_max));
-    t.push_params(P::new("Atk Power Normal Lv.MAX", atk_max));
-    t.push_params(P::new("Attack type Normal", attack_type));
-    t.push_params(P::new("Special Ability Normal", abilities));
+    t.push_params(P::new("Atk Range Normal", form.range));
+    t.push_params(P::new("Attack Frequency Normal", form.attack_cycle));
+    t.push_params(P::new("Movement Speed Normal", form.speed));
+    t.push_params(P::new("Knockback Normal", form.knockback));
+    t.push_params(P::new("Attack Animation Normal", form.animation));
+    t.push_params(P::new("Recharging Time Normal", form.recharge));
+    t.push_params(P::new("Hp Normal Lv.MAX", form.hp_max));
+    t.push_params(P::new("Atk Power Normal Lv.MAX", form.atk_max));
+    t.push_params(P::new("Attack type Normal", form.attack_type));
+    t.push_params(P::new("Special Ability Normal", form.abilities));
 
     println!("{t}");
 }
