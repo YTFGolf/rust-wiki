9f2e477464dc6794c95f298f29471c98d07f7215
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2025-06-02 20:02:18 +0100

    Add all forms

--- src/interface/scripts/cat_info/stats/template.rs
+++ src/interface/scripts/cat_info/stats/template.rs
@@ -1,3 +1,5 @@
+use std::iter::zip;
+
 use super::form::Form;
 use crate::{
     game_data::cat::parsed::cat::Cat,
@@ -6,7 +8,7 @@ use crate::{
     wikitext::template::{Template, TemplateParameter},
 };
 
-pub fn write_stats(t: &mut Template, form_name: &str, form: Form) {
+fn write_stats(t: &mut Template, form_name: &str, form: Form) {
     type P = TemplateParameter;
     let f = form_name;
 
@@ -22,20 +24,54 @@ pub fn write_stats(t: &mut Template, form_name: &str, form: Form) {
     t.push_params(P::new(format!("Special Ability {f}"), form.abilities));
 }
 
-pub fn get_template(cat: Cat) {
-    let stats = &cat.forms.stats[0];
-    let anims = &cat.forms.anims[0];
-
-    let mut t = Template::named("Cat Stats");
+fn add_all_forms(t: &mut Template, cat: &Cat) {
     type P = TemplateParameter;
 
+    let mut iter = zip(&cat.forms.stats, &cat.forms.anims).take(cat.forms.amt_forms);
+
+    let Some((stats, anims)) = iter.next() else {
+        return;
+    };
     let name = &CAT_DATA.get_cat(cat.id).normal;
     t.push_params(P::new("Normal Form name", name));
 
     let form = get_form(&cat, stats, anims);
     t.push_params(P::new("Hp Normal", form.base_hp));
     t.push_params(P::new("Atk Power Normal", form.base_atk));
-    write_stats(&mut t, "Normal", form.other);
+    write_stats(t, "Normal", form.other);
+
+    let Some((stats, anims)) = iter.next() else {
+        return;
+    };
+
+    let name = &CAT_DATA.get_cat(cat.id).evolved.as_ref().unwrap();
+    t.push_params(P::new("Evolved Form name", *name));
+    let form = get_form(&cat, stats, anims);
+    write_stats(t, "Evolved", form.other);
+
+    let Some((stats, anims)) = iter.next() else {
+        return;
+    };
+
+    let name = &CAT_DATA.get_cat(cat.id).true_form.as_ref().unwrap();
+    t.push_params(P::new("True Form name", *name));
+    let form = get_form(&cat, stats, anims);
+    write_stats(t, "True", form.other);
+
+    let Some((stats, anims)) = iter.next() else {
+        return;
+    };
+
+    let name = &CAT_DATA.get_cat(cat.id).ultra.as_ref().unwrap();
+    t.push_params(P::new("Ultra Form name", *name));
+    let form = get_form(&cat, stats, anims);
+    write_stats(t, "Ultra", form.other);
+}
+
+pub fn get_template(cat: Cat) {
+    let mut t = Template::named("Cat Stats");
+
+    add_all_forms(&mut t, &cat);
 
     println!("{t}");
 }
