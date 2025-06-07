6d42f460ef244845417d99c8e36060a8ec6eecd2
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2025-06-07 14:52:44 +0100

    Improve config getter

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,17 @@ fn initialise_config() -> ! {
     exit(0)
 }
 
+fn get_config() -> Config {
+    let file_content = &Config::read_config_file().unwrap_or_else(|| initialise_config());
+    match toml::from_str(&file_content) {
+        Ok(config) => config,
+        Err(e) => panic!("Error when parsing config: {e}"),
+    }
+}
+
 fn main() {
     let cli = Cli::parse();
-    let config: Config =
-        toml::from_str(&Config::read_config_file().unwrap_or_else(|| initialise_config())).unwrap();
+    let config = get_config();
 
     init_logger();
     unsafe { set_log_level(config.log_level) };
