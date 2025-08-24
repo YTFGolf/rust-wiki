cbbf3f6d41d12e855e620bfff875868565a2a244
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2025-08-24 22:13:59 +0100

    Fix incorrect error message

--- src/interface/scripts/cat_info/stats/form.rs
+++ src/interface/scripts/cat_info/stats/form.rs
@@ -65,7 +65,7 @@ pub fn get_form(
     let attack_length = stats.attack.hits.attack_length();
     assert!(
         anims.attack.length() > attack_length,
-        "Attack animation length mismatch: {} < {attack_length}",
+        "Attack animation length mismatch: {} <= {attack_length}",
         anims.attack.length()
     );
     let backswing = anims.attack.length() - attack_length;
