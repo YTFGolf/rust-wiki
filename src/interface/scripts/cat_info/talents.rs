8530c5ef89454bcc98f35b8fda33f78a1695dc04
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2025-10-03 00:27:32 +0100

    Add mini-surge

--- src/interface/scripts/cat_info/talents.rs
+++ src/interface/scripts/cat_info/talents.rs
@@ -2,6 +2,8 @@
 
 use std::fmt::Write;
 
+use num_format::{Locale, ToFormattedString};
+
 use crate::{
     game_data::cat::{
         parsed::{
@@ -20,13 +22,55 @@ use crate::{
 // use the description number?
 fn talent_from_text_id(talent: &SingleTalent, new_targets: &str) -> Option<String> {
     log::debug!("{talent:?}/{new_targets}");
+
+    let c_abil = usize::from(talent.ability_id);
+    // check ability id
+    let p_len = talent.params.len();
+
+    /// Assert that the minimum parameter is actually the same as the maximum.
+    /// Also return the min/max.
+    macro_rules! min_is_max {
+        ($id:expr) => {{
+            assert_eq!(talent.params[$id].0, talent.params[$id].1);
+            talent.params[$id].0
+        }};
+    }
+
     match talent.skill_description_id {
         1 => {
-            // weaken
-            assert_eq!(usize::from(talent.ability_id), 1_usize);
-            assert_eq!(talent.params.len(), 3);
+            // weaken, ???
+            assert_eq!(c_abil, 1);
+            assert_eq!(p_len, 3);
 
             todo!()
+        }
+        89 => {
+            // Mini-surge, level up for higher chance
+            assert_eq!(c_abil, 65);
+            assert_eq!(p_len, 4);
+
+            // chance,level,spawn_quad,range
+            let (min, max) = talent.params[0];
+            let level = min_is_max!(1);
+            let spawn_quad = min_is_max!(2);
+            let range_quad = min_is_max!(3);
+
+            let rng_min = spawn_quad / 4;
+            let rng_max = rng_min + range_quad / 4;
+
+            let step = {
+                let s = max - min;
+                let t = u16::from(talent.max_level) - 1;
+                assert_eq!(s % t, 0);
+                s / t
+            };
+
+            let msg = format!(
+                "Adds a {min}% chance to create a level {level} mini-surge between {rng1}~{rng2} range, improves by {step}% per level up to {max}%",
+                rng1 = rng_min.to_formatted_string(&Locale::en),
+                rng2 = rng_max.to_formatted_string(&Locale::en)
+            );
+            Some(msg)
         }
         id => {
             log::warn!("Unknown skill description id: {id}");
@@ -45,7 +89,7 @@ fn get_single_talent(talent: &SingleTalent, config: &Config, targs: &[TalentTarg
     // }
 
     let mut buf = format!(
-        "'''{}'''",
+        "*'''{}'''",
         TALENT_DATA.get_talent_name(talent.ability_id.into())
     );
 
@@ -81,9 +125,9 @@ fn get_single_talent(talent: &SingleTalent, config: &Config, targs: &[TalentTarg
         .unwrap()
         .costs;
     if costs.len() == 1 {
-        write!(buf, " (Cost: {})", costs[0]).infallible_write();
+        write!(buf, " (Cost: {} NP)", costs[0]).infallible_write();
     } else {
-        write!(buf, " (Total cost: {})", costs.iter().sum::<u16>()).infallible_write();
+        write!(buf, " (Total Cost: {} NP)", costs.iter().sum::<u16>()).infallible_write();
     }
 
     buf
