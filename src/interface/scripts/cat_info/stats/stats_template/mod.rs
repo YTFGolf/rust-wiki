40d6a38f971a9a3e52806b4079f7bf083fc9265c
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2025-10-22 18:06:47 +0100

    Add spirit stats

--- src/interface/scripts/cat_info/stats/stats_template/mod.rs
+++ src/interface/scripts/cat_info/stats/stats_template/mod.rs
@@ -1,6 +1,7 @@
 //! Deals with the various cat stats templates.
 
 pub mod manual;
+pub mod spirit;
 pub mod ver_0o1;
 pub mod ver_0o2;
 pub mod ver_1o0;
---
+++ src/interface/scripts/cat_info/stats/stats_template/spirit.rs
@@ -0,0 +1,39 @@
+//! Get manual Cat Stats template.
+
+use crate::{
+    game_data::cat::parsed::cat::Cat,
+    interface::scripts::cat_info::{form_util::CatForm, stats::form::get_form},
+    wikitext::template::{Template, TemplateParameter},
+};
+
+/// Get manual cat stats template.
+pub fn stats_spirit(cat: &Cat) -> Template {
+    type P = TemplateParameter;
+    const KAMIKAZE: &str = "{{AbilityIcon|Kamikaze}} [[Special Abilities#Kamikaze|Kamikaze]] (Attacks once, then disappears from the battlefield)<br>\n";
+
+    let mut t = Template::named("SpiritStats");
+    assert_eq!(cat.forms.amt_forms, 1);
+    let (stats, anims) = (&cat.forms.stats[0], &cat.forms.anims[0]);
+
+    let id = cat.id;
+    t.push_params(P::new("Spirit CRO", format!("{id:03}")));
+    t.push_params(P::new("Spirit Image", format!("{id:03} 1.png")));
+
+    let form = get_form(cat, stats, anims, CatForm::Normal);
+    t.push_params(P::new("Spirit Atk", form.other.atk_max));
+    t.push_params(P::new("Spirit Range", form.other.range));
+    t.push_params(P::new("Spirit Animation", form.other.animation));
+    t.push_params(P::new(
+        "Spirit Target",
+        form.other.attack_type.to_owned() + " Attack",
+    ));
+
+    let abilities = form.other.abilities;
+    assert!(abilities.starts_with(KAMIKAZE));
+    t.push_params(P::new(
+        "Spirit Ability",
+        abilities[KAMIKAZE.len()..].to_owned(),
+    ));
+
+    t
+}
