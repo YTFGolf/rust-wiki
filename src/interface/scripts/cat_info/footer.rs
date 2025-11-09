54a523a90756695fd6839b106a5ffdd72f71ee41
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2025-11-09 14:46:17 +0000

    Add footer docs

--- src/interface/scripts/cat_info/footer.rs
+++ src/interface/scripts/cat_info/footer.rs
@@ -1,7 +1,9 @@
+//! Footer section of cat page.
+
 use crate::{Config, game_data::cat::parsed::cat::Cat, wikitext::section::Section};
 
 /// Get footer section of page.
 pub fn footer(_cat: &Cat, _config: &Config) -> Section {
-    // TODO categories, see #
+    // TODO categories, see #19
     Section::blank("{{Cats}}")
 }
